bb460d7a459346f06a3201897528fcab4a612d63
skylen15 <khoidev.1512@outlook.com>
2026-09-19 13:15:58 +0700

    feat: add oxfmt as the workspace formatter

--- src/server/script.ts
+++ src/server/script.ts
@@ -1,8 +1,8 @@
-import { gs, type GlideRecord } from '@servicenow/glide'
+import { gs, type GlideRecord } from "@servicenow/glide";
 
 export function showStateUpdate(current: GlideRecord, previous: GlideRecord) {
-    const currentState = current.getValue('state')
-    const previousState = previous.getValue('state')
+  const currentState = current.getValue("state");
+  const previousState = previous.getValue("state");
 
-    gs.addInfoMessage(`state updated from "${previousState}" to "${currentState}"`)
+  gs.addInfoMessage(`state updated from "${previousState}" to "${currentState}"`);
 }
